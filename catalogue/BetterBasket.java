379f3c1f219c243e22d79daee4d7405eb914c823
Calactic <unconfigured@null.spigotmc.org>
2023-12-19 09:28:56 +0000

    BetterBasked implementation and JUnit testing

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -4,15 +4,29 @@ import java.io.Serializable;
 import java.util.Collections;
 
 /**
- * Write a description of class BetterBasket here.
+ * Improved version of the Basket class with recommended additions
  * 
- * @author  Your Name 
+ * @author  Callum Barnett
  * @version 1.0
  */
 public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
   
-
-  // You need to add code here
+  @Override
+  public boolean add(Product pr) {
+	  for(Product existingProduct: this) {
+		  if(existingProduct.getProductNum().equals(pr.getProductNum())) {
+			  System.out.println("Product is repeated");
+			  existingProduct.setQuantity(existingProduct.getQuantity() + pr.getQuantity());
+			  return true;
+		  }
+	  }
+	  System.out.println("Product is not repeated");
+	  super.add(pr);
+	  return true;
+  }
+  public void sortBasket() {
+	  Collections.sort(this, (p1, p2) -> p1.getProductNum().compareTo(p2.getProductNum()));
+  }
 }
